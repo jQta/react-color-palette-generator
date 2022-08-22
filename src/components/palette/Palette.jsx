7014a5d3212264a43c70520932d721d9c5d16375
jQta <estevezpereira@gmail.com>
2022-08-22 21:56:52 +0200

    test saved palettes

--- src/components/palette/Palette.jsx
+++ src/components/palette/Palette.jsx
@@ -1,10 +1,12 @@
 import "./Palette.scss";
 import { CompactPicker } from "react-color";
 import { useState } from "react";
+import trash from "../../assets/icons/trash.svg";
 
 export default function Palette() {
   const [selector, setSelector] = useState({
     activeObject: null,
+    title: "",
     objects: [
       { id: 1, color: { backgroundColor: "#999999" } },
       { id: 2, color: { backgroundColor: "#999999" } },
@@ -36,6 +38,21 @@ export default function Palette() {
     backgroundColor: color,
   };
 
+  const [input, setInput] = useState();
+  const [paletteSaved, setPaletteSaved] = useState([]);
+  const [paletteList, setPaletteList] = useState([]);
+
+  function checkInput() {
+    if (input === undefined) {
+      console.log("escribe algo");
+    } else {
+      setPaletteSaved({ ...selector, title: input });
+      setPaletteList([...paletteList, paletteSaved]);
+    }
+  }
+  console.log("selector", selector);
+  console.log(paletteList);
+
   return (
     <div className="palette-box">
       <div className="palette-box__selector">
@@ -49,10 +66,26 @@ export default function Palette() {
       <div className="searcher-box">
         <h2 className="searcher-box__title">Name</h2>
         <div className="searcher-box__tools">
-          <input className="searcher-box__tools--input" placeholder="Website color scheme" />
-          <button className="searcher-box__tools--add">+</button>
+          <input className="searcher-box__tools--input" placeholder="Website color scheme" onChange={e => setInput(e.target.value)} />
+          <button className="searcher-box__tools--add" onClick={checkInput}>
+            +
+          </button>
+        </div>
+        <div>
+          <h2>Saved palettes</h2>
+          {paletteList.map((item, index) => (
+            <div key={index}>
+              <h3>{item.title}</h3>
+              <img src={trash} alt="delete" />
+              {/* {item.objects.map((obj, index) => (
+                <div key={index} />
+              ))} */}
+            </div>
+          ))}
         </div>
       </div>
     </div>
   );
 }
+
+
